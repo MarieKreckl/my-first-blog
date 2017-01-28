3334d36e32823e851cf8d4fb6f1723667a478a49
Marie Kreckl <m.kreckl@t-online.de>
2017-01-28 12:54:18 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,23 @@
+print('Hello, Django girls!')
+
+
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Rachel")
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for girl in girls
+
+for x in girls
+	hi()
